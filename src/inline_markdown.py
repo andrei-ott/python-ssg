aee30f0c800aa9eba08839e1636e663e5638d561
thorfinn-karlsefn <andrei.ott@docusketch.com>
2024-11-25 19:51:35 +0400

    add images and links extract

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import *
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -23,3 +24,9 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.extend(splitted_nodes)
     
     return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
