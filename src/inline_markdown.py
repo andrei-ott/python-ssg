2dcd03e3190a266856b0ad57006ccdfeefb9eec3
thorfinn-karlsefn <andrei.ott@docusketch.com>
2024-11-25 20:15:36 +0400

    add text_to_textnodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,6 +1,19 @@
 from textnode import *
 import re
 
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.NORMAL)]
+
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_image(nodes)
+
+    return nodes
+
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -4,7 +4,8 @@ from inline_markdown import (
     extract_markdown_images,
     extract_markdown_links,
     split_nodes_link,
-    split_nodes_image
+    split_nodes_image,
+    text_to_textnodes
 )
 
 from textnode import TextNode, TextType
@@ -168,6 +169,26 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" with text that follows", TextType.NORMAL),
             ],
             new_nodes,
+        )
+
+
+    def test_split_links(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.NORMAL),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.NORMAL),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.NORMAL),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.NORMAL),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
         )
 
 
