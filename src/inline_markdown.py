e7754eab27b53f90b33a0f3848d33a4c8b38612c
thorfinn-karlsefn <andrei.ott@docusketch.com>
2024-11-25 20:08:18 +0400

    add image and link splitting

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -25,8 +25,62 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     
     return new_nodes
 
+
 def extract_markdown_images(text):
     return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
+
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+        else:
+            splitted_texts = re.split(r"(?<!!)\[(.*?)\]\((.*?)\)", node.text)
+            
+            splitted_nodes = []
+            link_text = ""
+            for i in range(0, len(splitted_texts)):
+                text = splitted_texts[i]
+                if len(text) == 0:
+                    continue
+
+                if i % 3 == 0:
+                    splitted_nodes.append(TextNode(text, TextType.NORMAL))
+                elif i % 3 == 1:
+                    link_text = text
+                else:
+                    splitted_nodes.append(TextNode(link_text, TextType.LINK, text))
+            new_nodes.extend(splitted_nodes)
+    
+    return new_nodes
+
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.NORMAL:
+            new_nodes.append(node)
+        else:
+            splitted_texts = re.split(r"!\[(.*?)\]\((.*?)\)", node.text)
+            
+            splitted_nodes = []
+            image_text = ""
+            for i in range(0, len(splitted_texts)):
+                text = splitted_texts[i]
+                if len(text) == 0:
+                    continue
+
+                if i % 3 == 0:
+                    splitted_nodes.append(TextNode(text, TextType.NORMAL))
+                elif i % 3 == 1:
+                    link_text = text
+                else:
+                    splitted_nodes.append(TextNode(link_text, TextType.IMAGE, text))
+            new_nodes.extend(splitted_nodes)
+    
+    return new_nodes
