b774298f1e57f924f04125c3eaa3a9a6ab17e166
thorfinn-karlsefn <andrei.ott@docusketch.com>
2024-11-25 17:35:28 +0400

    add leaf noe

---
+++ src/test_leafnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_values(self):
+        props = { "href": "url", "target": "_blank" }
+        a_node = LeafNode("a", "This is a link", props)
+
+        self.assertEqual(a_node.tag, "a")
+        self.assertEqual(a_node.value, "This is a link")
+        self.assertEqual(a_node.children, None)
+        self.assertEqual(a_node.props, props)
+
+    def test_to_html(self):
+        a_node = LeafNode("a", "This is a link", { "href": "url", "target": "_blank" })
+
+        self.assertEqual(a_node.to_html(), "<a href=\"url\" target=\"_blank\">This is a link</a>")
+
+    def test_to_html_without_tag(self):
+        node = LeafNode(None, "This is a link")
+
+        self.assertEqual(node.to_html(), "This is a link")
+
+    def test_to_html_value_error(self):
+        node = LeafNode(None, None)
+
+        self.assertRaises(ValueError, node.to_html)
+
+
+if __name__ == "__main__":
+    unittest.main()
